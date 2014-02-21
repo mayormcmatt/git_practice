c86b280cb2a448524835447807620063bbe32436
kevinrogerteng <kevin.roger.teng@gmail.com>
2014-02-21 14:33:52 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitty::Application.config.secret_key_base = '97279d0991bd145583994b62c7cb67a78387e6565378b46bcb33968ba2cdb8b6771c23679789f0b89ed3b128880e28fb18933e1f5fa387b58bd2065465053b7a'
